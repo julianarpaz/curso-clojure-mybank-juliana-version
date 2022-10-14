f0706948a51109ccba57a0470812139267b02247
Allef Santana <allefff@gmail.com>
2022-10-14 14:38:20 -0300

    Testando saldo suficiente

--- test/mybank_web_api/core_test.clj
+++ test/mybank_web_api/core_test.clj
@@ -13,6 +13,22 @@
                true   (is-a-valid-number? "120")
                true   (is-a-valid-number? "120.0"))))
 
+(deftest saldo-suficiente?-test
+  (let [conta {:saldo 100}]
+    (testing "Check success"
+      ;(are [x] (saldo-suficiente? conta x)
+      ;            true (saldo-suficiente? conta 1)
+      ;            true (saldo-suficiente? conta 99)
+      ;            true (saldo-suficiente? conta 100))
+      (are [x] (saldo-suficiente? conta x)
+               1
+               99
+               100))
+      (is (= true (saldo-suficiente? conta 1)))
+      (is (= true (saldo-suficiente? conta 99)))
+      (is (= true (saldo-suficiente? conta 100)))
+    (testing "Check failure"
+      (is (= false (saldo-suficiente? conta 110))))))
 
 (deftest positive?-test
   (testing "Checking if the number is greater than zero"
